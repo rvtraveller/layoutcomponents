8dd765fe7f9d2102fcdc7fca6f88d1c038b3552a
David Biescas <david.biescas.gaspar@everis.com>
2020-12-10 08:11:52 -0500

    Carousel js fixes

--- modules/lc_simple_view_carousel/js/lc_simple_view_carousel.js
+++ modules/lc_simple_view_carousel/js/lc_simple_view_carousel.js
@@ -15,30 +15,36 @@
   Drupal.behaviors.lcHomeCarrousel = {
     attach: function (context) {
       $(document).once('init').ready(function () {
-        var $slick = $('div[data-quickedit-field-id^="' + drupalSettings.homeCarousel.id + '"]').find('.slick__slider');
-        var $dots = $slick.find('ul').addClass('slick-dots-tabs').find('li[role="presentation"]');
-        $dots.each(function (i) {
-          var $text = '<div class="slick-dot-number">(' + i + ') </div>';
-          $text += '<div class="slick-dot-text">' + drupalSettings.homeCarousel.items[i].title + '</div>';
-          $($dots[i]).find('button').html($text);
-        });
+        if (typeof drupalSettings.homeCarousel != 'undefined') {
+          var $slick = $('.view-home-carousel').find('.slick__slider');
+          var $dots = $slick.find('ul').addClass('slick-dots-tabs').find('li[role="presentation"]');
+          $dots.each(function (i) {
+            var $text = '<div class="slick-dot-number">' + (i + 1) + ' </div>';
+            $text += '<div class="slick-dot-text">' + drupalSettings.homeCarousel.items[i].title + '</div>';
+            $($dots[i]).find('button').html($text);
+          });
 
-        var $isPaused = ($slick.hasClass('is-paused')) ? Drupal.t('Play') : Drupal.t('Pause');
+          var $isPaused = ($slick.hasClass('is-paused')) ? Drupal.t('Play') : Drupal.t('Pause');
 
-        var $li = $('<li class="slick-dot-control">' + $isPaused + '</li>').on('click', function () {
+          var $li = $('<li class="slick-dot-control slick-play-active">' + $isPaused + '</li>').on('click', function () {
 
-          // Movement control.
-          if ($slick.hasClass('is-paused')) {
-            $(this).text(Drupal.t('Pause'));
-            $slick.removeClass('is-paused').slick('slickPlay');
-          } else {
-            $(this).text(Drupal.t('Play'));
-            $slick.addClass('is-paused').slick('slickPause');
-          }
-        })
+            // Movement control.
+            if ($slick.hasClass('is-paused')) {
+              $(this).text(Drupal.t('Pause'));
+              $slick.removeClass('is-paused').slick('slickPlay');
+              $li.addClass('slick-play-active');
+              $li.removeClass('slick-play-pause');
+            } else {
+              $(this).text(Drupal.t('Play'));
+              $slick.addClass('is-paused').slick('slickPause');
+              $li.removeClass('slick-play-active');
+              $li.addClass('slick-play-pause');
+            }
+          })
 
-        // Inlude Play/Pause button.
-        $slick.find('ul').once('added').append($li);
+          // Inlude Play/Pause button.
+          $slick.find('ul').once('added').append($li);
+        }
       })
     }
   };
